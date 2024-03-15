d8ba24f8a29f18de2126f9422ce8ac80ff1b8af9
hd2yao <1054290355@qq.com>
2024-03-15 16:58:21 +0800

    feat:data_race_01.go

---
+++ Week03/blog/03_data_race/01/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+var wg sync.WaitGroup
+var counter int
+
+func main() {
+    for i := 0; i < 10; i++ {
+        run()
+    }
+}
+
+func run() {
+    for i := 0; i <= 2; i++ {
+        wg.Add(1)
+        go routine(i)
+    }
+    wg.Wait()
+    fmt.Printf("Final Counter: %d\n", counter)
+}
+
+func routine(id int) {
+    for i := 0; i < 2; i++ {
+        value := counter
+        value++
+        counter = value
+    }
+    wg.Done()
+}
